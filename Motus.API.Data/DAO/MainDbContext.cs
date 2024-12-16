d7dcb0c9fe6af6e797de38b7eed2ba0d3f8666d4
mkrtc <mkrtc@USER>
2024-12-16 17:59:40 +0400

    Add project files.

---
+++ Motus.API.Data/DAO/MainDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Motus.API.Data.DAO {
+    public class MainDbContext : DbContext {
+        public MainDbContext(DbContextOptions options) : base(options) { }
+
+        // Declaring the tables of database
+        //public DbSet<RandomEntity> Randoms { get; set; }
+    }
+}
---
+++ Motus.API.Data/Migrations/20241213132253_initial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Motus.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Randoms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Randoms", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Randoms");
+        }
+    }
+}
